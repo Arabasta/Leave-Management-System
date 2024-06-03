87a81b0203c267aa2c319bbf571f44a6ab807974
Ke <83902652+Arabasta@users.noreply.github.com>
2024-06-04 06:31:25 +0800

    Initial Commit: added dependencies and structured project directory

---
+++ src/main/java/com/team4/leaveprocessingsystem/LeaveProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.team4.leaveprocessingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveProcessingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveProcessingSystemApplication.class, args);
+    }
+
+}
