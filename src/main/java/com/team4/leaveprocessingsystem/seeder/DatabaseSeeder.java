67d47f14ce875f3888bc1fa77de11016661e2595
Arabasta <83902652+Arabasta@user.noreply.github.com>
2024-06-07 16:14:26 +0800

    refactor: REMOVED unused import from DatabaseSeeder

--- src/main/java/com/team4/leaveprocessingsystem/seeder/DatabaseSeeder.java
+++ src/main/java/com/team4/leaveprocessingsystem/seeder/DatabaseSeeder.java
@@ -1,6 +1,5 @@
 package com.team4.leaveprocessingsystem.seeder;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
