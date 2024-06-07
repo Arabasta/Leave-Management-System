5b3d67aa68193db8768855dfe904ca2054aabdfe
Arabasta <83902652+Arabasta@user.noreply.github.com>
2024-06-07 16:56:03 +0800

    refactor: REMOVED unused import statement

--- src/main/java/com/team4/leaveprocessingsystem/interfacemethods/IUser.java
+++ src/main/java/com/team4/leaveprocessingsystem/interfacemethods/IUser.java
@@ -1,7 +1,6 @@
 package com.team4.leaveprocessingsystem.interfacemethods;
 
 import com.team4.leaveprocessingsystem.model.User;
-import jakarta.transaction.Transactional;
 
 public interface IUser {
     boolean save(User user);
