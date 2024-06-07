c12c3935953503fda2b3f81875ca54a850cc2859
Arabasta <83902652+Arabasta@user.noreply.github.com>
2024-06-07 11:19:53 +0800

    <type>: <Subject> (Max 50 char, start UPPERCASE) fix: REMOVE unused imports

--- src/main/java/com/team4/leaveprocessingsystem/LeaveProcessingSystemApplication.java
+++ src/main/java/com/team4/leaveprocessingsystem/LeaveProcessingSystemApplication.java
@@ -1,13 +1,9 @@
 package com.team4.leaveprocessingsystem;
 
-import com.team4.leaveprocessingsystem.model.User;
-import com.team4.leaveprocessingsystem.model.enums.AccessLevelEnum;
-import com.team4.leaveprocessingsystem.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class LeaveProcessingSystemApplication {
@@ -16,17 +12,6 @@ public class LeaveProcessingSystemApplication {
         SpringApplication.run(LeaveProcessingSystemApplication.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner commandLineRunner(UserRepository ur, PasswordEncoder passwordEncoder) {
-//        return args -> {
-//            if (ur.count() == 0) {
-//                ur.save(
-//                        new User(AccessLevelEnum.ROLE_ADMIN,
-//                                "root",
-//                                passwordEncoder.encode("root"),
-//                                "root@gmail.com"));
-//            }
-//        };
-//    }
+
 }
 
