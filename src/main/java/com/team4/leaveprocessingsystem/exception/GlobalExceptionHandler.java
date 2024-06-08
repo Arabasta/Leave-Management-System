41a224c5e35384f003050a015172b0b79ac04304
Arabasta <keiyamx@gmail.com>
2024-06-09 07:54:24 +0800

    refactor: RENAMED Exceptions dir to exception
    Signed-off-by: Arabasta <keiyamx@gmail.com>

--- src/main/java/com/team4/leaveprocessingsystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/team4/leaveprocessingsystem/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.team4.leaveprocessingsystem.Exceptions;
+package com.team4.leaveprocessingsystem.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
