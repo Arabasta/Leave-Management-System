6653e361d342f2f3912ba6b165a5deaf91658100
Jefri <jefritoo@gmail.com>
2024-06-08 23:41:36 +0800

    fix: wrong implementation of validator fixed

--- src/main/java/com/team4/leaveprocessingsystem/validator/LeaveApplicationValidator.java
+++ src/main/java/com/team4/leaveprocessingsystem/validator/LeaveApplicationValidator.java
@@ -1,10 +1,12 @@
-package com.team4.leaveprocessingsystem.service;
+package com.team4.leaveprocessingsystem.validator;
 
-import com.team4.leaveprocessingsystem.interfacemethods.IValidator;
 import com.team4.leaveprocessingsystem.model.LeaveApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
-public class LeaveApplicationValidator implements IValidator {
+@Component
+public class LeaveApplicationValidator implements Validator {
     @Override
     public boolean supports(Class<?> clazz) {
         return LeaveApplication.class.isAssignableFrom(clazz);
