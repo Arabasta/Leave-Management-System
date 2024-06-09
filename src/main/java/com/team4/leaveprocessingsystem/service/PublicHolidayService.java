21593c0bb33392cea68e548f310f428f957ee062
Arabasta <keiyamx@gmail.com>
2024-06-09 08:31:52 +0800

    feat: ADDED findByDateAndHoliday for PublicHoliday
    Signed-off-by: Arabasta <keiyamx@gmail.com>

--- src/main/java/com/team4/leaveprocessingsystem/service/PublicHolidayService.java
+++ src/main/java/com/team4/leaveprocessingsystem/service/PublicHolidayService.java
@@ -1,12 +1,15 @@
 package com.team4.leaveprocessingsystem.service;
 
-import com.team4.leaveprocessingsystem.Exceptions.ServiceSaveException;
+import com.team4.leaveprocessingsystem.exception.ServiceSaveException;
 import com.team4.leaveprocessingsystem.interfacemethods.IPublicHoliday;
 import com.team4.leaveprocessingsystem.model.PublicHoliday;
 import com.team4.leaveprocessingsystem.repository.PublicHolidayRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class PublicHolidayService implements IPublicHoliday {
@@ -22,5 +25,9 @@ public class PublicHolidayService implements IPublicHoliday {
         } catch (Exception e) {
             throw new ServiceSaveException(publicHoliday.getHoliday(), e);
         }
+    }
+
+    public Optional<PublicHoliday> findByDateAndHoliday(LocalDate date, String holiday) {
+        return publicHolidayRepository.findByDateAndHoliday(date, holiday);
     }
 }
