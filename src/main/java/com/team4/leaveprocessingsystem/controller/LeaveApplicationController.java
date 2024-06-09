ea2c8b1029cfb5a94623144624bc00e986ec3482
Jefri <jefritoo@gmail.com>
2024-06-09 11:34:12 +0800

    feat: implemented create/edit/delete/cancelled logic

--- src/main/java/com/team4/leaveprocessingsystem/controller/LeaveApplicationController.java
+++ src/main/java/com/team4/leaveprocessingsystem/controller/LeaveApplicationController.java
@@ -8,11 +8,13 @@ import com.team4.leaveprocessingsystem.model.enums.LeaveStatusEnum;
 import com.team4.leaveprocessingsystem.model.enums.LeaveTypeEnum;
 import com.team4.leaveprocessingsystem.service.EmployeeService;
 import com.team4.leaveprocessingsystem.service.LeaveApplicationService;
+import com.team4.leaveprocessingsystem.validator.LeaveApplicationValidator;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("leave")
@@ -22,20 +24,17 @@ public class LeaveApplicationController {
     private ILeaveApplication leaveService;
     @Autowired
     private EmployeeService employeeService;
-
     @Autowired
     public void setLeaveService(LeaveApplicationService leaveService){
         this.leaveService = leaveService;
     }
+    @Autowired
+    private LeaveApplicationValidator leaveApplicationValidator;
 
-    // Pending PR merge
-//    @Autowired
-//    private LeaveApplicationValidator leaveApplicationValidator;
-//
-//    @InitBinder("course")
-//    private void initLeaveApplicationBinder(WebDataBinder binder) {
-//        binder.addValidators(leaveApplicationValidator);
-//    }
+    @InitBinder
+    private void initLeaveApplicationBinder(WebDataBinder binder) {
+        binder.addValidators(leaveApplicationValidator);
+    }
 
     @GetMapping("create")
     public String createLeave(Model model){
@@ -45,47 +44,61 @@ public class LeaveApplicationController {
 
         model.addAttribute("leave", leaveApplication);
         model.addAttribute("leaveTypes", LeaveTypeEnum.values());
+        model.addAttribute("formAction", "/leave/create");
         //TODO: Get all applied/updated/approved leave to prevent overlap
 
         return "leaveApplication/leaveForm";
     }
 
     @PostMapping("create")
-    public String saveCreatedLeave(@Valid @ModelAttribute("leave") LeaveApplication leaveApplication, BindingResult bindingResult){
+    public String saveCreatedLeave(@Valid @ModelAttribute("leave") LeaveApplication leaveApplication, BindingResult bindingResult,Model model){
+        Employee employee = employeeService.findByName("employee"); // Replace with session get employee obj
+
         if (bindingResult.hasErrors()) {
+            leaveApplication.setSubmittingEmployee(employee);
+
+            model.addAttribute("leave", leaveApplication);
+            model.addAttribute("leaveTypes", LeaveTypeEnum.values());
             return "leaveApplication/leaveForm";
         }
-        Employee employee = employeeService.findByName("employee"); // Replace with session get employee obj
-        Manager manager = employee.getManager();
 
+        Manager manager = employee.getManager();
         leaveApplication.setSubmittingEmployee(employee);
         leaveApplication.setReviewingManager(manager);
         leaveApplication.setLeaveStatus(LeaveStatusEnum.APPLIED);
         leaveService.save(leaveApplication);
 
-        return "leaveApplication/viewLeaveHistory";
+        return "redirect:/history";
     }
 
     @GetMapping("edit/{id}")
     public String editLeave(@PathVariable int id, Model model){
         model.addAttribute("leave", leaveService.findLeaveApplicationById(id));
-
+        model.addAttribute("leaveTypes", LeaveTypeEnum.values());
+        model.addAttribute("formAction", "/leave/edit");
         //TODO: Get all applied/updated/approved leave to prevent overlap
 
         return "leaveApplication/leaveForm";
     }
 
-    @PostMapping("edit/{id}")
-    public String saveEditedLeave(@Valid @ModelAttribute("leave") LeaveApplication leaveApplication, BindingResult bindingResult){
+    @PostMapping("edit")
+    public String saveEditedLeave(@Valid @ModelAttribute("leave") LeaveApplication leaveApplication, BindingResult bindingResult, Model model){
+        Employee employee = employeeService.findByName("employee"); // Replace with session get employee obj
         if (bindingResult.hasErrors()) {
+            leaveApplication.setSubmittingEmployee(employee);
+
+            model.addAttribute("leave", leaveApplication);
+            model.addAttribute("leaveTypes", LeaveTypeEnum.values());
             return "leaveApplication/leaveForm";
         }
 
         //TODO: Applied/Updated leave can be updated
-
+        Manager manager = employee.getManager();
+        leaveApplication.setSubmittingEmployee(employee);
+        leaveApplication.setReviewingManager(manager);
         leaveApplication.setLeaveStatus(LeaveStatusEnum.UPDATED);
         leaveService.save(leaveApplication);
-        return "leaveApplication/viewLeaveHistory";
+        return "redirect:/history";
     }
 
     @GetMapping("delete/{id}")
@@ -94,19 +107,26 @@ public class LeaveApplicationController {
         leaveApplication.setLeaveStatus(LeaveStatusEnum.DELETED);
         leaveService.save(leaveApplication);
 
-        //TODO: Applied/updated leave can be deleted
+        //TODO: Only applied/updated leave can be deleted
 
-        return "leaveApplication/viewLeaveHistory";
+        return "redirect:/history";
     }
 
     @GetMapping("cancel/{id}")
     public String cancelLeave(@PathVariable int id){
         LeaveApplication leaveApplication = leaveService.findLeaveApplicationById(id);
-        leaveApplication.setLeaveStatus(LeaveStatusEnum.DELETED);
+        leaveApplication.setLeaveStatus(LeaveStatusEnum.CANCELLED);
         leaveService.save(leaveApplication);
 
-        //TODO: Applied/updated/approved leave can be cancelled
+        //TODO: Only approved leave can be cancelled
 
+        return "redirect:/history";
+    }
+
+    @GetMapping("history")
+    public String leaveHistory(Model model){
         return "leaveApplication/viewLeaveHistory";
     }
+
+
 }
--- src/main/java/com/team4/leaveprocessingsystem/validator/LeaveApplicationValidator.java
+++ src/main/java/com/team4/leaveprocessingsystem/validator/LeaveApplicationValidator.java
@@ -1,10 +1,12 @@
-package com.team4.leaveprocessingsystem.service;
+package com.team4.leaveprocessingsystem.validator;
 
-import com.team4.leaveprocessingsystem.interfacemethods.IValidator;
 import com.team4.leaveprocessingsystem.model.LeaveApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
-public class LeaveApplicationValidator implements IValidator {
+@Component
+public class LeaveApplicationValidator implements Validator {
     @Override
     public boolean supports(Class<?> clazz) {
         return LeaveApplication.class.isAssignableFrom(clazz);
@@ -13,8 +15,8 @@ public class LeaveApplicationValidator implements IValidator {
     @Override
     public void validate(Object target, Errors errors) {
         LeaveApplication application = (LeaveApplication) target;
-        if ((application.getStartDateTime() != null) && (application.getEndDateTime() != null) &&
-                (application.getStartDateTime().isAfter(application.getEndDateTime()))) {
+        if ((application.getStartDate() != null) && (application.getEndDate() != null) &&
+                (application.getStartDate().isAfter(application.getEndDate()))) {
             errors.rejectValue("endDate", "error.dates",
                     "End Date must be later than Start Date");
         }
