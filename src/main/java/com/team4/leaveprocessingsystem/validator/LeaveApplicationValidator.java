a3f720d019e4aafc8f6cbac29ab3221d242975ac
Jefri <jefritoo@gmail.com>
2024-06-10 20:29:41 +0800

    feat: ADD validation for start/end date must be a working day

--- src/main/java/com/team4/leaveprocessingsystem/validator/LeaveApplicationValidator.java
+++ src/main/java/com/team4/leaveprocessingsystem/validator/LeaveApplicationValidator.java
@@ -1,12 +1,21 @@
 package com.team4.leaveprocessingsystem.validator;
 
 import com.team4.leaveprocessingsystem.model.LeaveApplication;
+import com.team4.leaveprocessingsystem.service.PublicHolidayService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+
 @Component
 public class LeaveApplicationValidator implements Validator {
+    @Autowired
+    private PublicHolidayService publicHolidayService;
+
     @Override
     public boolean supports(Class<?> clazz) {
         return LeaveApplication.class.isAssignableFrom(clazz);
@@ -15,10 +24,27 @@ public class LeaveApplicationValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         LeaveApplication application = (LeaveApplication) target;
-        if ((application.getStartDate() != null) && (application.getEndDate() != null) &&
-                (application.getStartDate().isAfter(application.getEndDate()))) {
-            errors.rejectValue("endDate", "error.dates",
-                    "End Date must be later than Start Date");
+        LocalDate startDate = application.getStartDate();
+        LocalDate endDate = application.getEndDate();
+
+        if (startDate != null && endDate != null) {
+
+            List<LocalDate> publicHolidayList = publicHolidayService.publicHolidayDateList();
+
+            if (publicHolidayList.contains(startDate) ||
+                    startDate.getDayOfWeek() == DayOfWeek.SATURDAY || startDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                errors.rejectValue("startDate", "error.dates", "Start date should be a working day");
+            }
+
+            if (publicHolidayList.contains(endDate) ||
+                    endDate.getDayOfWeek() == DayOfWeek.SATURDAY || endDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                errors.rejectValue("endDate", "error.dates", "End date should be a working day");
+            }
+
+            if (startDate.isAfter(endDate)) {
+                errors.rejectValue("endDate", "error.dates",
+                        "End date must be later than Start Date");
+            }
         }
     }
 }
