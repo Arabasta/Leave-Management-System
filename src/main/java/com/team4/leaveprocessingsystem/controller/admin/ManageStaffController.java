661248500542671638cd0cdcb65daaa4df492b8e
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-06-10 21:38:28 +0800

    feat and refactor: added update url and changed mapping methods
    - bug: /update

--- src/main/java/com/team4/leaveprocessingsystem/controller/admin/ManageStaffController.java
+++ src/main/java/com/team4/leaveprocessingsystem/controller/admin/ManageStaffController.java
@@ -1,28 +1,28 @@
 package com.team4.leaveprocessingsystem.controller.admin;
 
 import com.team4.leaveprocessingsystem.interfacemethods.IEmployee;
-import com.team4.leaveprocessingsystem.model.Employee;
-import com.team4.leaveprocessingsystem.model.User;
+import com.team4.leaveprocessingsystem.model.*;
 import com.team4.leaveprocessingsystem.model.enums.RoleEnum;
 import com.team4.leaveprocessingsystem.repository.UserRepository;
 import com.team4.leaveprocessingsystem.service.JobDesignationService;
-import lombok.Setter;
+
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@Setter
 @Controller
+@RequestMapping(value = "/admin/manage-staff")
 public class ManageStaffController {
+
+    //todo: field injection is not recommended
     @Autowired
     private IEmployee employeeService;
     @Autowired
@@ -30,7 +30,7 @@ public class ManageStaffController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping(value = "/search-staff")
+    @GetMapping("/")
     public String search(@RequestParam(value = "keyword", required = false) String k,
                          @RequestParam(value = "searchtype", required = false) String t,
                          Model model) {
@@ -52,37 +52,60 @@ public class ManageStaffController {
                 model.addAttribute("staffs", employeeService.findUserByRoleType(k));
                 break;
             default:
-                return "errorStaffNotFound";
+                return "error/404-notfound";
         }
         model.addAttribute("keyword", k);
         model.addAttribute("searchtype", t);
-        return "searchStaffResults";
+        return "manage-staff/view-all";
     }
 
-    @RequestMapping(value = "edit/{id}")
-    public ModelAndView showEditStaffForm(@PathVariable(name = "id") int id, Model model) {
-        BiFunction<List<User>, String, Boolean> employeeHasThisUserRoleFlag = (listOfUserRoles, specificRole) -> {
-            return listOfUserRoles.stream().anyMatch(user -> user.getRole().toString().equals(specificRole));
+    @GetMapping("/edit/{employeeId}")
+    public String editEmployeeDetails(@PathVariable(name = "employeeId") int employeeId, Model model) {
+
+        Employee employee = employeeService.findEmployeeById(employeeId);
+
+        BiFunction<List<User>, String, Boolean> userHasThisRole = (employeeUserAccounts, userAccountRole) -> {
+            return employeeUserAccounts.stream().anyMatch(userAccount -> userAccount.getRole().toString().equals(userAccountRole));
         };
 
-        List<User> employeeUserAccountList = userRepository.findUserRolesByEmployeeId(id);
-
-        List<String> roleEnumNamesStrings = Stream.of(RoleEnum.values())
+        List<String> roleEnumsAsStrings = Stream.of(RoleEnum.values())
                 .map(RoleEnum::name)
                 .collect(Collectors.toList());
 
-        model.addAttribute("flag", employeeHasThisUserRoleFlag);
+        List<User> userAccountListByEmployeeId = userRepository.findUserRolesByEmployeeId(employeeId);
 
-        ModelAndView mav = new ModelAndView("editStaff");
-        Employee employee = employeeService.findEmployeeById(id);
+        List<JobDesignation> jobDesignationList = jobDesignationService.listAllJobDesignations();
 
-        mav.addObject("employee", employee);
-        mav.addObject("jobDesignations", jobDesignationService.listAllJobDesignations());
+        model.addAttribute("employee", employee);
+        model.addAttribute("userRoleflag", userHasThisRole);
+        model.addAttribute("roles", roleEnumsAsStrings);
+        model.addAttribute("userAccountListByEmployeeId", userAccountListByEmployeeId);
+        model.addAttribute("jobDesignationList", jobDesignationList);
 
-        mav.addObject("roles", roleEnumNamesStrings);
-        mav.addObject("employeeUserAccountList", userRepository.findUserRolesByEmployeeId(id));
+        model.addAttribute("isEditMode", true);
 
-        return mav;
+        return "manage-staff/edit-employee-details-form";
+    }
+
+    @PostMapping("/update")
+    public String updateEmployeeDetails(@RequestParam("employeeId") int employeeId,
+                                        @Valid @ModelAttribute("employee") Employee employee,
+                                        BindingResult bindingResult,
+                                        Model model
+                                        ) {
+        employeeService.save(employee); // todo: fix bug
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("There are validation errors.");
+            model.addAttribute("employee", employee);
+            return "manage-staff/edit-employee-details-form";
+        }
+        // todo: save the role of the employee?
+
+        model.addAttribute("isEditMode", false);
+        model.addAttribute("updateSuccess", true);
+
+        return "redirect:/admin/manage-staff/view-all";
     }
 
 }
