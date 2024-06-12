8c2f5a89faecf9b927e4598708814962b4d3b843
Arabasta <keiyamx@gmail.com>
2024-06-12 17:47:31 +0800

    feat: ADDED AuthenticationService for retrieving loggedin user's id

---
+++ src/main/java/com/team4/leaveprocessingsystem/service/AuthenticationService.java
@@ -0,0 +1,27 @@
+package com.team4.leaveprocessingsystem.service;
+
+import com.team4.leaveprocessingsystem.model.Employee;
+import com.team4.leaveprocessingsystem.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    public int getLoggedInEmployeeId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            User user = (User) authentication.getPrincipal();
+            Employee employee = user.getEmployee();
+            if (employee != null) {
+                return employee.getId();
+            }
+            throw new IllegalStateException("Employee not found for the logged-in user");
+        }
+        return -1;
+    }
+
+}
