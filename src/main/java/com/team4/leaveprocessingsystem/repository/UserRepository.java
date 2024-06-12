c29b4604d084d22dd78c821ad42a5a202a99ef52
Arabasta <keiyamx@gmail.com>
2024-06-12 11:10:55 +0800

    fix: QUERIES for employee and user repositories

--- src/main/java/com/team4/leaveprocessingsystem/repository/UserRepository.java
+++ src/main/java/com/team4/leaveprocessingsystem/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.team4.leaveprocessingsystem.repository;
 import com.team4.leaveprocessingsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,7 +11,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 
-    @Query("Select u from User u join u.employee userEmployee where userEmployee.id = :k")
-    List<User> findUserRolesByEmployeeId(@Param("k") Integer employeeId);
+    @Query("SELECT u FROM User u WHERE u.employee.id = :employeeId")
+    List<User> findUserRolesByEmployeeId(Integer employeeId);
 
 }
