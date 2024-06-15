fc1f13938b0957cb4deac7ac2568cd52b945f9e7
Jefri <jefritoo@gmail.com>
2024-06-15 12:25:28 +0800

    add: Allow http requests with authorization headers for API consumption

--- src/main/java/com/team4/leaveprocessingsystem/security/WebSecurityConfig.java
+++ src/main/java/com/team4/leaveprocessingsystem/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,6 +51,7 @@ public class WebSecurityConfig {
                         .anyRequest().authenticated()
 
                 )
+                .httpBasic(Customizer.withDefaults()) //For API consumption, allow http requests with Authorization header
                 // successful login redirection path
                 .formLogin((form) -> form
                         .loginPage("/auth/login")
