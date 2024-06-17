9a2e29b8ee526bc0fe7aad3bfaa88af4d52ffc29
luca <xiongkun778@163.com>
2024-06-17 23:36:17 +0800

    fix: redo newUserForm.js to make sure 2 password match then can create/update

--- src/main/resources/static/js/newUserForm.js
+++ src/main/resources/static/js/newUserForm.js
@@ -1,25 +1,29 @@
 // Testing onChange() does not work
 // todo: fix bug password validation does not work
-function onChange() {
-    const password = document.querySelector('input[name=userPassword]');
-    const confirm = document.querySelector('input[name=confirmPassword]');
-    if (confirm.value === password.value) {
-        confirm.setCustomValidity('');
+
+
+function checkPasswords() {
+    const password = document.getElementById('userPassword');
+    const confirm = document.getElementById('confirmPassword');
+    const message = document.getElementById('message');
+
+    if (password.value === confirm.value) {
+        message.textContent = 'Passwords match';
+        message.style.color = 'green';
     } else {
-        confirm.setCustomValidity('Passwords do not match');
+        message.textContent = 'Passwords do not match';
+        message.style.color = 'red';
     }
 }
 
-var check = function() {
-    const password = document.getElementById("userPassword");
-    const confirm = document.getElementById("confirmPassword");
+function validatePasswordMatch() {
+    const password = document.getElementById('userPassword');
+    const confirm = document.getElementById('confirmPassword');
 
-    if (password.value === confirm.value) {
-        document.getElementById("message").style.color = "green";
-        document.getElementById("message").innerHTML = "Passwords match!"
-    } else {
-        document.getElementById("message").style.color = "red";
-        document.getElementById("message").innerHTML = "Passwords do not match!"
+    if (password.value !== confirm.value) {
+        alert('Passwords do not match!');
+        return false;
     }
+    return true;
 }
 
