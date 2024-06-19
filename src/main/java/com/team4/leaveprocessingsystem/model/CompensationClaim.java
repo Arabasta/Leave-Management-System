8be6162b4e8b8e21342c0c306a505b6daa556ec4
zephyrdark <zephyrdark@users.noreply.github.com>
2024-06-19 20:26:09 +0800

    Refactor: Compensation Claim Model - Add Jakarta Bean Validation

--- src/main/java/com/team4/leaveprocessingsystem/model/CompensationClaim.java
+++ src/main/java/com/team4/leaveprocessingsystem/model/CompensationClaim.java
@@ -2,6 +2,8 @@ package com.team4.leaveprocessingsystem.model;
 
 import com.team4.leaveprocessingsystem.model.enums.CompensationClaimStatusEnum;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -24,26 +26,32 @@ public class CompensationClaim {
     @ManyToOne(optional = false)
     private Employee claimingEmployee;
 
+    @PastOrPresent
     @Column(nullable = false)
     private LocalDateTime claimDateTime;
 
+    @PastOrPresent
     @Column(nullable = false)
     @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime overtimeStart;
 
+    @PastOrPresent
     @Column(nullable = false)
     @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime overtimeEnd;
 
+    @Positive
     @Column(nullable = false)
     private float overtimeHours;
 
+    @Positive
     @Column(nullable = false)
     private float compensationLeaveRequested;
 
     @ManyToOne(optional = false)
     private Manager approvingManager;
 
+    @PastOrPresent
     private LocalDateTime reviewedDateTime;
 
     private String comments;
