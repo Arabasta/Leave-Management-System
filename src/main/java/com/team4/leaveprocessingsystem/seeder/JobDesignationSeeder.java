ab7592d1d46a82772eb8fc2a2edfa261fac12380
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-06-20 08:55:55 +0800

    refactor: remove all files for manage leave type use case

--- src/main/java/com/team4/leaveprocessingsystem/seeder/JobDesignationSeeder.java
+++ src/main/java/com/team4/leaveprocessingsystem/seeder/JobDesignationSeeder.java
@@ -1,80 +1,35 @@
 package com.team4.leaveprocessingsystem.seeder;
 
 import com.team4.leaveprocessingsystem.model.JobDesignation;
-import com.team4.leaveprocessingsystem.model.LeaveType;
 import com.team4.leaveprocessingsystem.repository.JobDesignationRepository;
-import com.team4.leaveprocessingsystem.repository.LeaveTypeRepository;
-import com.team4.leaveprocessingsystem.service.LeaveTypeService;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class JobDesignationSeeder {
 
     private final JobDesignationRepository jobDesignationRepository;
-    private final LeaveTypeRepository leaveTypeRepository;
-    private final LeaveTypeService leaveTypeService;
 
-    public JobDesignationSeeder(JobDesignationRepository jobDesignationRepository,
-                                LeaveTypeRepository leaveTypeRepository, LeaveTypeService leaveTypeService) {
+    public JobDesignationSeeder(JobDesignationRepository jobDesignationRepository) {
         this.jobDesignationRepository = jobDesignationRepository;
-        this.leaveTypeRepository = leaveTypeRepository;
-        this.leaveTypeService = leaveTypeService;
+
     }
 
     public void seed() {
         if (jobDesignationRepository.count() == 0) {
 
-            // Create Leave Type List for Job Designation initialization
-            LeaveType annualLeave = leaveTypeService.findLeaveTypeById(1);
-            LeaveType medicalLeave = leaveTypeService.findLeaveTypeById(2);
-            LeaveType compensationLeave = leaveTypeService.findLeaveTypeById(3);
-            LeaveType compassionateLeave = leaveTypeService.findLeaveTypeById(4);
-            LeaveType unpaidLeave = leaveTypeService.findLeaveTypeById(5);
-
-            // ************************ ADMINISTRATIVE **********************************
-            List<LeaveType> leaveTypesForAdmininstrative = new ArrayList<LeaveType>();
-            leaveTypesForAdmininstrative.add(annualLeave);
-            leaveTypesForAdmininstrative.add(medicalLeave);
-            leaveTypesForAdmininstrative.add(compassionateLeave);
-            leaveTypesForAdmininstrative.add(unpaidLeave);
-
             JobDesignation administrative = new JobDesignation("administrative",
-                    14,
-                    leaveTypesForAdmininstrative);
+                    14);
 
             jobDesignationRepository.save(administrative);
 
-            // ************************ MANAGEMENT **********************************
-            List<LeaveType> leaveTypesForManagement = new ArrayList<LeaveType>();
-            leaveTypesForManagement.add(annualLeave);
-            leaveTypesForManagement.add(medicalLeave);
-            leaveTypesForManagement.add(compassionateLeave);
-            leaveTypesForManagement.add(unpaidLeave);
-
-            JobDesignation management = new JobDesignation("management", 30, leaveTypesForManagement);
-
+            JobDesignation management = new JobDesignation("management", 30);
             jobDesignationRepository.save(management);
 
-            // ************************ CLEANING **********************************
-            List<LeaveType> leaveTypesForCleaning = new ArrayList<LeaveType>();
-            leaveTypesForCleaning.add(annualLeave);
-            leaveTypesForCleaning.add(medicalLeave);
-            leaveTypesForCleaning.add(compassionateLeave);
-
-            JobDesignation cleaning = new JobDesignation("cleaning", 3, leaveTypesForCleaning);
+            JobDesignation cleaning = new JobDesignation("cleaning", 3);
             jobDesignationRepository.save(cleaning);
 
-            // ************************ INTERN **********************************
-            List<LeaveType> leaveTypesForIntern = new ArrayList<LeaveType>();
-            leaveTypesForCleaning.add(annualLeave);
-            leaveTypesForCleaning.add(medicalLeave);
-            leaveTypesForCleaning.add(compassionateLeave);
-            leaveTypesForCleaning.add(compensationLeave);
 
-            JobDesignation intern = new JobDesignation("intern", 10, leaveTypesForCleaning);
+            JobDesignation intern = new JobDesignation("intern", 10);
             jobDesignationRepository.save(intern);
         }
     }
