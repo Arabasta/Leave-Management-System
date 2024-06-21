3cbda9775fed46c9525747f2b80b93bbfd0eee30
Arabasta <keiyamx@gmail.com>
2024-06-22 01:16:11 +0800

    feat: LEAVEBALANCE entity added constraint @Min to leave fields

--- src/main/java/com/team4/leaveprocessingsystem/model/LeaveBalance.java
+++ src/main/java/com/team4/leaveprocessingsystem/model/LeaveBalance.java
@@ -2,6 +2,7 @@ package com.team4.leaveprocessingsystem.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,29 +14,37 @@ public class LeaveBalance {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Min(value = 0, message = "Cannot be negative")
     @Column(nullable = false)
     private float annualLeave;
 
+    @Min(value = 0, message = "Cannot be negative")
     @Column(nullable = false)
     private float currentAnnualLeave;
 
+    @Min(value = 0, message = "Cannot be negative")
     @Column(nullable = false)
     private float medicalLeave = 60;
 
+    @Min(value = 0, message = "Cannot be negative")
     @Column(nullable = false)
     private float currentMedicalLeave = medicalLeave;
 
     public static final float COMPASSIONATE_LEAVE = 4;
 
+    @Min(value = 0, message = "Cannot be negative")
     @Column(nullable = false)
     private float compassionateLeaveConsumed = 0;
 
+    @Min(value = 0, message = "Cannot be negative")
     @Column(nullable = false)
     private float compensationLeave = 0;
 
+    @Min(value = 0, message = "Cannot be negative")
     @Column(nullable = false)
-    private float currentCompensationLeave = compensationLeave;
 
+    private float currentCompensationLeave = compensationLeave;
+    @Min(value = 0, message = "Cannot be negative")
     @Column(nullable = false)
     private float unpaidLeaveConsumed = 0;
 
